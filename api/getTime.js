720e6de7a6940a6338e5d2165305b7f605ceacdb
hama77 <mountasargharsalli@gmail.com>
2026-04-14 21:09:53 +0100

    Update getTime.js

--- api/getTime.js
+++ api/getTime.js
@@ -1,3 +1,22 @@
+import crypto from "crypto";
+
+const KEY = Buffer.from("KMdaF2HeNUT0ye6N3LvF0Mso");
+const IV = Buffer.from("45760761".padEnd(16, "0"));
+
+function encrypt(text) {
+  const cipher = crypto.createCipheriv("aes-192-cbc", KEY, IV);
+  let encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  return encrypted;
+}
+
+function decrypt(encryptedText) {
+  const decipher = crypto.createDecipheriv("aes-192-cbc", KEY, IV);
+  let decrypted = decipher.update(encryptedText, "base64", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
+
 export default function handler(req, res) {
   try {
     const jsonData = JSON.stringify({
