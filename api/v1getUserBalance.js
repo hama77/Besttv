e121cae5d769741bd7e0b266ce287103fdb0b30b
hama77 <mountasargharsalli@gmail.com>
2026-04-17 17:33:19 +0100

    Add user balance API with encryption

---
+++ api/v1getUserBalance.js
@@ -0,0 +1,25 @@
+import crypto from "crypto";
+
+const KEY = Buffer.from("KMdaF2HeNUT0ye6N3LvF0Mso");
+const IV = Buffer.from("45760761".padEnd(16, "0"));
+
+function encrypt(text) {
+  const cipher = crypto.createCipheriv("aes-192-cbc", KEY, IV);
+  let encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  return encrypted;
+}
+
+export default function handler(req, res) {
+  const jsonData = JSON.stringify({
+    code: 200,
+    msg: "success",
+    data: {
+      balance: 150,
+      currency: "USD",
+      vip: true
+    }
+  });
+
+  res.status(200).send(encrypt(jsonData));
+}
