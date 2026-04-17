e5a332800bbdccc2e02680662321552b6d823e58
hama77 <mountasargharsalli@gmail.com>
2026-04-17 17:50:48 +0100

    Add encryption handler for payment order response

---
+++ api/v1payBesttvOrderXX.js
@@ -0,0 +1,25 @@
+import crypto from "crypto";
+
+const KEY = Buffer.from("KMdaF2HeNUT0ye6N3LvF0Mso");
+const IV = Buffer.from("45760761".padEnd(16, "0"));
+
+function encrypt(text) {
+  const cipher = crypto.createCipheriv("aes-192-cbc", KEY, IV);
+  let encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  return encrypted;
+}
+
+export default function handler(req, res) {
+  const jsonData = JSON.stringify({
+    code: 200,
+    msg: "success",
+    data: {
+      orderId: "ORD123456",
+      status: "paid",
+      amount: 10
+    }
+  });
+
+  res.status(200).send(encrypt(jsonData));
+}
