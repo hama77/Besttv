982a97e74a48a3cc45e4be1f6a9b5c314df87a94
hama77 <mountasargharsalli@gmail.com>
2026-04-17 13:48:50 +0100

    Add Config class with API endpoint constants
    Replaced the previous JavaScript handler with a new Config class containing API endpoint constants.

---
+++ Config.java
@@ -0,0 +1,36 @@
+package com.your.app;
+
+public class Config {
+
+    public static final String API_GET_TIME = "https://besttv-424r.vercel.app/api/v1/getTime";
+    public static final String API_USER_BALANCE = "https://besttv-424r.vercel.app/api/v1/getUserBalance";
+    public static final String API_RENEWAL = "https://besttv-424r.vercel.app/api/v1/getBesttvRenewalScheme";
+    public static final String API_PAY_ORDER = "https://besttv-424r.vercel.app/api/payBesttvOrderXX";
+    public static final String API_SET_PASSWORD = "https://besttv-424r.vercel.app/api/v1/setPaymentPassword";
+
+    public static final String DEX_URL_1 = "https://raw.githubusercontent.com/hama77/Besttv/main/dex/module1.dex";
+    public static final String DEX_URL_2 = "https://raw.githubusercontent.com/hama77/Besttv/main/dex/module2.dex";
+
+    public static final String[] ENDPOINTS = {
+        API_GET_TIME,       // Index 0
+        API_USER_BALANCE,   // Index 1
+        API_RENEWAL,        // Index 2
+        API_PAY_ORDER,      // Index 3
+        API_SET_PASSWORD,   // Index 4
+        DEX_URL_1,          // Index 5
+        DEX_URL_2,          // Index 6
+        "https://link7.com",
+        "https://link8.com",
+        "https://link9.com",
+        "https://link10.com",
+        "https://link11.com",
+        "https://link12.com",
+        "https://link13.com",
+        "https://link14.com",
+        "https://link15.com",
+        "https://link16.com",
+        "https://link17.com",
+        "https://link18.com",
+        "https://link19.com"
+    };
+}
