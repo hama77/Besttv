8e4ceb24a97302d31ee50cd44da90a62b3b90179
hama77 <mountasargharsalli@gmail.com>
2026-04-17 15:01:00 +0100

    Implement password encryption in setPaymentPassword handler

---
+++ api/v1/setPaymentPassword.js
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+
+const KEY = Buffer.from("KMdaF2HeNUT0ye6N3LvF0Mso");
+const IV = Buffer.from("45760761".padEnd(16, "0"));
+
+function encrypt(text) {
+  const cipher = crypto.createCipheriv("aes-192-cbc", KEY, IV);
+  let encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  return encrypted;
+}
+
+export default function handler(req, res) {
+  const jsonData = JSON.stringify({
+    code: 200,
+    msg: "password set success",
+    data: true
+  });
+
+  res.status(200).send(encrypt(jsonData));
+}
